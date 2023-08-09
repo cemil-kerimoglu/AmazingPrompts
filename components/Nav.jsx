b79f223dbc30b88e1eb34680154317a1804490bc
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-08-09 15:25:12 +0200

    establish authorization

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,8 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -35,7 +36,7 @@ const Nav = () => {
 
       {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -45,7 +46,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -72,14 +73,14 @@ const Nav = () => {
 
       {/*Mobile Navigation*/}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               alt="AmazingPrompts Logo"
               width={30}
               height={30}
-              className="object-contain"
+              className="rounded-full"
               onClick={() => setToggleDropdown((prev) => !prev)}
             />
             {toggleDropdown && (
